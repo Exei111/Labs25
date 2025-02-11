8048684176ed90affb642b981d79daba99983721
Exei111 <shniperovmatvey@gmail.com>
2025-02-11 16:31:22 +0300

    123

---
+++ build/mymath/CMakeFiles/mymath.dir/mymath.cpp.o.d
@@ -0,0 +1,4 @@
+mymath/CMakeFiles/mymath.dir/mymath.cpp.o: \
+ /home/exeiqq/Shniperov-CS-Labs-2024/mymath/mymath.cpp \
+ /usr/include/stdc-predef.h \
+ /home/exeiqq/Shniperov-CS-Labs-2024/mymath/mymath.h
